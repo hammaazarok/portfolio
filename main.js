f52a4f5a62290486d51bf9e61c7ed397a0c6fd49
Hamma Azarok <hammaagazarok@gmail.com>
2022-06-14 11:34:13 +0200

    fix linter errors

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 const body = document.querySelectorAll('.wrapper,.header');
 
 const mobileMenu = () => {
-    navMenu.classList.toggle("show");
-   // hamburger.classList.toggle("show");
-    if(navMenu.classList.contains("show")){
-        
+  navMenu.classList.toggle('show');
+  // hamburger.classList.toggle("show");
+  if (navMenu.classList.contains('show')) {
     hamburger.innerHTML = '<i class="fas fa-times"  style="color:#fff"></i>';
-}else{
+  } else {
     hamburger.innerHTML = '<i class="fas fa-bars"></i>';
-}
-   
-    body.forEach(n=> n.classList.toggle("bgcolor"));
+  }
 
-}
+  body.forEach((n) => n.classList.toggle('bgcolor'));
+};
 
 const closeMenu = () => {
-    hamburger.innerHTML = "<i class='fas fa-bars'></i>";
-    navMenu.classList.remove("show");
-    hamburger.classList.remove("show");
-    body.forEach(n=> n.classList.remove("bgcolor"))
-}
+  hamburger.innerHTML = "<i class='fas fa-bars'></i>";
+  navMenu.classList.remove('show');
+  hamburger.classList.remove('show');
+  body.forEach((n) => n.classList.remove('bgcolor'));
+};
 
-const navLink = document.querySelectorAll(".nav-link");
-navLink.forEach( n => n.addEventListener("click", closeMenu));
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
 
-hamburger.addEventListener("click", mobileMenu, false);
-
-
+hamburger.addEventListener('click', mobileMenu, false);
