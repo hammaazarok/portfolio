6eb490888e1f8bf678f051c79381417e37a37af7
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-06-16 12:01:52 +0100

    Add form-validatio.js file

--- form-validation.js
+++ form-validation.js
@@ -1,5 +1,21 @@
-function formValidation(){
-//write code here
+const form = document.getElementById('form-button');
+
+form.addEventListener("click", function (event) {
+    const email = document.getElementById('email-field');
+    const emailValue = email.value;
+ if(emailValue.toLowerCase() != emailValue) {
+    let error_message = "Error: form is not sent. Make sure the email is in lowercase"
+    form.setCustomValidity(error_message);
+    form.reportValidity();
+    return;
+}else{
+    let error_message = ""
+    email.setCustomValidity(error_message);
+    email.reportValidity(); 
+}
+console.log(emailValue);
+console.log(emailValue.toLowerCase());
 }
 
-formValidation();
+)
+
