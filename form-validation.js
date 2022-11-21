947afefc77b988106d2dcb53cef443c0bf3af913
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-21 22:13:16 +0200

    add email sending functionality with nodejs

--- form-validation.js
+++ form-validation.js
@@ -4,14 +4,34 @@ let errorMessage = '';
 function isLowerCase(str) {
   return str === str.toLowerCase() && str !== str.toUpperCase();
 }
-form.addEventListener('click', () => {
+form.addEventListener('click', (e) => {
+  const name = document.getElementById('name-field');
   const email = document.getElementById('email-form');
+  const message = document.getElementById('message');
   const emailValue = email.value;
-  console.log(isLowerCase(String(emailValue)));
   if (isLowerCase(String(emailValue)) === false) {
     errorMessage = 'Error: form is not sent. Make sure the email is in lowercase';
     formContainer.innerHTML = errorMessage;
   } else {
-    formContainer.innerHTML = '';
+    e.preventDefault();
+    fetch('http://hammadevs.ly/email', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ Email: email.value, Name: name.value, Message: message.value }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        // console.log(response);
+        if (response.status === 200) {
+          formContainer.innerHTML = `${response.message}`;
+        } else {
+          // formContainer.innerHTML = 'Sorry an Expected error occured';
+          console.log(response.message);
+          formContainer.innerHTML = `${response.message}`;
+        }
+      });
   }
 });
